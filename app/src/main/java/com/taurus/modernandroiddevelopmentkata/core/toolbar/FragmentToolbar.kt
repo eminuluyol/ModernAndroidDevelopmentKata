447fa2c3f2ef98c33ceeb71f85a5a29eec5a92c0
eminuluyol <eminuluyol@gmail.com>
2019-01-30 13:27:05 +0100

    Improve Fragment toolbar visibility

--- app/src/main/java/com/taurus/modernandroiddevelopmentkata/core/toolbar/FragmentToolbar.kt
+++ app/src/main/java/com/taurus/modernandroiddevelopmentkata/core/toolbar/FragmentToolbar.kt
@@ -6,56 +6,51 @@ import androidx.annotation.StringRes
 @DslMarker
 annotation class ToolbarDSL
 
-class FragmentToolbar constructor(
-    @IdRes val resId: Int,
-    @StringRes val title: Int,
-    val isOnHomePressedDefaultAction: Boolean,
-    val isTransparentStatusBar: Boolean
+class FragmentToolbar private constructor(
+        @IdRes val resId: Int,
+        @StringRes val title: Int,
+        val isOnHomePressedDefaultAction: Boolean,
+        val isTransparentStatusBar: Boolean
 ) {
 
-  companion object {
-    const val NO_TOOLBAR = -1
-    const val NO_TITLE = -1
+    companion object {
+        const val NO_TOOLBAR = -1
+        const val NO_TITLE = -1
 
-    fun decorateToolbar(@IdRes resId: Int,
-        func: FragmentToolbar.Builder.() -> Unit): FragmentToolbar {
-      return Builder()
-          .withId(resId)
-          .apply(func)
-          .build()
-    }
-  }
-
-  @ToolbarDSL
-  class Builder {
-    private var resId: Int = NO_TOOLBAR
-    private var title: Int = -1
-    private var noTitle = false
-    private var isOnHomePressedDefaultAction = false
-    private var isTransparentStatusBar = false
-
-    @ToolbarDSL
-    fun withId(@IdRes resId: Int) = apply { this.resId = resId }
-
-    @ToolbarDSL
-    fun withTitle(title: Int) = apply { this.title = title }
-
-    @ToolbarDSL
-    fun noToolbar() = apply { this.resId = NO_TOOLBAR }
-
-    @ToolbarDSL
-    fun onHomePressedDefaultAction() = apply {
-      this.isOnHomePressedDefaultAction = true
+        fun decorateToolbar(@IdRes resId: Int,
+                            func: FragmentToolbar.Builder.() -> Unit): FragmentToolbar {
+            return Builder()
+                    .withId(resId)
+                    .apply(func)
+                    .build()
+        }
     }
 
     @ToolbarDSL
-    fun withTransparentStatusBar() = apply { this.isTransparentStatusBar = true }
+    class Builder {
+        private var resId: Int = NO_TOOLBAR
+        private var title: Int = -1
+        private var noTitle = false
+        private var isOnHomePressedDefaultAction = false
+        private var isTransparentStatusBar = false
 
-    fun build(): FragmentToolbar = FragmentToolbar(
-        resId,
-        title,
-        isOnHomePressedDefaultAction,
-        isTransparentStatusBar
-    )
-  }
+        fun withId(@IdRes resId: Int) = apply { this.resId = resId }
+
+        fun withTitle(title: Int) = apply { this.title = title }
+
+        fun noToolbar() = apply { this.resId = NO_TOOLBAR }
+
+        fun onHomePressedDefaultAction() = apply {
+            this.isOnHomePressedDefaultAction = true
+        }
+
+        fun withTransparentStatusBar() = apply { this.isTransparentStatusBar = true }
+
+        fun build(): FragmentToolbar = FragmentToolbar(
+                resId,
+                title,
+                isOnHomePressedDefaultAction,
+                isTransparentStatusBar
+        )
+    }
 }
--- app/src/main/java/com/taurus/modernandroiddevelopmentkata/movie/MovieFragment.kt
+++ app/src/main/java/com/taurus/modernandroiddevelopmentkata/movie/MovieFragment.kt
@@ -6,14 +6,14 @@ import com.taurus.modernandroiddevelopmentkata.core.toolbar.FragmentToolbar
 
 class MovieFragment : BaseFragment<MovieStateMachine>() {
 
-  override fun getViewModel() = MovieStateMachine::class.java
+    override fun getViewModel() = MovieStateMachine::class.java
 
-  override fun layoutResId() = R.layout.fragment_movie
+    override fun layoutResId() = R.layout.fragment_movie
 
-  override fun toolbarBuilder(): FragmentToolbar {
-    return FragmentToolbar.decorateToolbar(R.id.toolbar) {
-      withTitle(R.string.nav_movies)
+    override fun toolbarBuilder(): FragmentToolbar {
+        return FragmentToolbar.decorateToolbar(R.id.toolbar) {
+            withTitle(R.string.nav_movies)
+        }
     }
-  }
 
 }
