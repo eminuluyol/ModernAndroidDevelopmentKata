ba428343ca48400ea3c58f64575091e7ce61ab4a
eminuluyol <eminuluyol@gmail.com>
2019-02-01 16:17:54 +0100

    Use generated directio classes for navigation and enable Kotli Extensions

--- app/src/main/java/com/taurus/modernandroiddevelopmentkata/movie/MovieFragment.kt
+++ app/src/main/java/com/taurus/modernandroiddevelopmentkata/movie/MovieFragment.kt
@@ -2,32 +2,30 @@ package com.taurus.modernandroiddevelopmentkata.movie
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import com.taurus.modernandroiddevelopmentkata.R
 import com.taurus.modernandroiddevelopmentkata.core.BaseFragment
 import com.taurus.modernandroiddevelopmentkata.core.toolbar.FragmentToolbar
-import kotlinx.android.synthetic.main.fragment_movie.showDetailButton
+import kotlinx.android.synthetic.main.fragment_movie.*
 
 class MovieFragment : BaseFragment<MovieStateMachine>() {
 
-  override fun obtainViewModel() = MovieStateMachine::class.java
+    override fun obtainViewModel() = MovieStateMachine::class.java
 
-  override fun layoutResId() = R.layout.fragment_movie
+    override fun layoutResId() = R.layout.fragment_movie
 
-  override fun toolbarBuilder(): FragmentToolbar {
-    return FragmentToolbar.decorateToolbar(R.id.toolbar) {
-      withTitle(R.string.movies)
+    override fun toolbarBuilder(): FragmentToolbar {
+        return FragmentToolbar.decorateToolbar(R.id.toolbar) {
+            withTitle(R.string.movies)
+        }
     }
-  }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    showDetailButton.setOnClickListener {
-      navController.navigate(
-          R.id.action_movies_to_detailFragment,
-          bundleOf("titleArg" to "From Movie Fragment")
-      )
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showDetailButton.setOnClickListener {
+            navController.navigate(
+                    MovieFragmentDirections.actionActionMoviesToDetailFragment("From Movie Fragment")
+            )
+        }
     }
-  }
 
 }
